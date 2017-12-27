c1d952bd78604674d74fa3b622b0837fcd79eb4c
mr_ <njgriffiths@gmail.com>
2017-12-26 22:15:07 -0500

    or there... :P

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const port = 3000;
 const express = require('express');
 
 const app = express()
-	.use(express.static('public'));
+	.use(express.static('public'))
 	.listen(port, function() {
 		console.log('Express server is up on port ' + port);
 	});
