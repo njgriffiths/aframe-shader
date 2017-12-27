2cd77484b3d110c40623baafa8654c77c59a201e
mr_ <njgriffiths@gmail.com>
2017-12-26 22:14:20 -0500

    shouldn't've have a semi-colon there...

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const port = 3000;
 const express = require('express');
 
-const app = express();
+const app = express()
 	.use(express.static('public'));
 	.listen(port, function() {
 		console.log('Express server is up on port ' + port);
