6fedc491c67a2af59bad722d44568b2bed1c3387
mr_ <njgriffiths@gmail.com>
2017-12-26 21:41:20 -0500

    add comment explaining 'vUv'

--- public/shaders/fragment.fs
+++ public/shaders/fragment.fs
@@ -10,7 +10,6 @@ void main() {
     //    (b) Base color.
     //
     // The color itself is a vec4 containing RGBA values 0-1.
-    
     gl_FragColor = mix(
         vec4(mod(vUv, 0.05) * 20.0, 1.0, 1.0),
         vec4(color, 1.0),
